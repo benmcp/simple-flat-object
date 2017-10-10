75429a08783c5dfec9fcc0702276aa878f223f87
Ben McPhail <ben@presentcompany.co>
2017-10-10 23:26:04 +1100

    Add eslint changes

--- src/index.js
+++ src/index.js
@@ -1,46 +1,56 @@
 class FlatObject {
-  static flatten(data) {
-    const result = {};
-    const recurse = (cur, prop) => {
-      if (Object(cur) !== cur) {
-        result[prop] = cur;
-      } else if (Array.isArray(cur)) {
-         for(var i=0, l=cur.length; i<l; i++)
-            recurse(cur[i], prop + "[" + i + "]");
-        if (l == 0)
-          result[prop] = [];
-      } else {
-        let isEmpty = true;
-        for (let p in cur) {
-          isEmpty = false;
-          recurse(cur[p], prop ? prop+"."+p : p);
-        }
-        if (isEmpty && prop)
-          result[prop] = {};
-      }
-    }
-    recurse(data, "");
-    return result;
-  }
+	static flatten(data) {
+		const result = {};
+		const recurse = (cur, prop) => {
+			let i;
+			let l;
 
-  static unflatten(data) {
-    "use strict";
-    if (Object(data) !== data || Array.isArray(data))
-      return data;
-    const regex = /\.?([^.\[\]]+)|\[(\d+)\]/g,
-      resultholder = {};
-    for (let p in data) {
-      let cur = resultholder,
-        prop = "",
-        m;
-      while (m = regex.exec(p)) {
-        cur = cur[prop] || (cur[prop] = (m[2] ? [] : {}));
-        prop = m[2] || m[1];
-      }
-      cur[prop] = data[p];
-    }
-    return resultholder[""] || resultholder;
-  }
+			if (Object(cur) !== cur) {
+				result[prop] = cur;
+			} else if (Array.isArray(cur)) {
+				for (i = 0, l = cur.length; i < l; i += 1) {
+					recurse(cur[i], `${prop}[${i}]`);
+				}
+				if (l === 0) {
+					result[prop] = [];
+				}
+			} else {
+				let isEmpty = true;
+				for (const p in cur) {
+					isEmpty = false;
+					recurse(cur[p], prop ? `${prop}.${p}` : p);
+				}
+				if (isEmpty && prop) {
+					result[prop] = {};
+				}
+			}
+		};
+
+		recurse(data, '');
+		return result;
+	}
+
+	static unflatten(data) {
+		if (Object(data) !== data || Array.isArray(data)) {
+			return data;
+		}
+
+		let p;
+		const regex = /\.?([^.\[\]]+)|\[(\d+)\]/g;
+		const resultholder = {};
+
+		for (p in data) {
+			let cur = resultholder;
+			let prop = '';
+			let m;
+			while (m = regex.exec(p)) {
+				cur = cur[prop] || (cur[prop] = (m[2] ? [] : {}));
+				prop = m[2] || m[1];
+			}
+			cur[prop] = data[p];
+		}
+		return resultholder[''] || resultholder;
+	}
 }
 
 export default FlatObject;
